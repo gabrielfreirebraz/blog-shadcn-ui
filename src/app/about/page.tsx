04542692bc9886f0607916297fb10e142277b519
Gabriel Braz <gabrielfreirebraz@gmail.com>
2024-10-06 17:15:07 -0300

    feat: new about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,27 +4,20 @@ import { config } from "@/config";
 import { signOgImageUrl } from "@/lib/og-image";
 import Markdown from "react-markdown";
 
-const content = `# About Me
 
-![Samantha](https://imagedelivery.net/lLmNeOP7HXG0OqaG97wimw/clvlugru90000o4g8ahxp069s/db7abbe3-aa5c-433e-a16d-cbf137d1c9e5.png/public)
 
-Hey there! I'm Samantha, a 28-year-old former corporate warrior who decided to ditch the 9-to-5 grind and embark on an adventure of a lifetime. After years of hustling in a high-pressure job, I realized that life is too short to be stuck in an office, staring at spreadsheets all day.
+const content = `# Sobre
 
-So I took a leap of faith, quit my cushy job in Singapore, and decided to see the world on my own terms. No more stuffy meetings or rigid schedules – just me, my backpack, and an open road ahead.
+Olá! Eu sou Bruna Brandão, blogueira nas horas vagas e entusiasta do mundo das finanças. Embora eu não seja uma especialista no assunto, sou apaixonada por aprender e compartilhar o que descubro sobre educação financeira, investimentos e gestão de finanças pessoais.
 
-![Samantha](https://imagedelivery.net/lLmNeOP7HXG0OqaG97wimw/clvlugru90000o4g8ahxp069s/6b080e65-2329-4a36-ad5c-0a6af8d9aeb1.png/public)
+O objetivo deste blog é dividir minhas experiências e aprendizados na minha jornada de conhecimento financeiro. Aqui, você encontrará artigos que exploram temas como economia, poupança, investimentos e planejamento financeiro, sempre com o foco em tornar esses assuntos mais acessíveis e compreensíveis para todos.
 
-This blog is where I'll be documenting my travels, sharing my experiences, and hopefully inspiring others to follow their wanderlust. From trekking through remote villages to savoring local cuisines, I'm on a mission to immerse myself in different cultures and create memories that will last a lifetime.
+A minha visão com este blog é criar um espaço onde possamos discutir e compartilhar ideias sobre como podemos melhorar nossa relação com o dinheiro, sempre de forma leve e sem complicações. Se você está buscando inspiração para sua própria jornada financeira, está no lugar certo!
 
-But this journey isn't just about checking off destinations from a bucket list. It's about self-discovery, personal growth, and finding the courage to live life on my own terms. I'll be honest and raw, sharing the highs and lows, the moments of pure bliss and the inevitable challenges that come with solo travel.
+![Bruna](http://localhost:3000/images/bruna.png)
 
-So join me on this adventure, and let's explore the world together! Who knows, maybe my stories will inspire you to take that leap of faith and pursue your own dreams, whatever they may be.
-
-Let's go on an adventure!
-
-Love,
-
-Samantha`;
+#### *Importante lembrar que o conteúdo deste blog reflete apenas minhas opiniões pessoais e não constitui recomendações financeiras ou de investimento. Sempre recomendo que você busque orientação de um especialista financeiro antes de tomar qualquer decisão relacionada a investimentos ou finanças.*
+`
 
 export async function generateMetadata() {
   return {
