1dd2db502ff16b5269df91e94e9e09d4a4cb79b2
Gabriel Braz <gabrielfreirebraz@gmail.com>
2024-10-12 18:23:56 -0300

    feat: add new logo version (v2)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -77,9 +77,9 @@ export const Navigation: FunctionComponent = () => {
 export const Header: FunctionComponent = () => {
   return (
     <section className="flex items-center justify-between mt-8 md:mt-16 mb-12">
-      <Link href="/">
+      <Link href="/" className="md:-mt-8">
         <h1 className="text-4xl md:text-6xl font-bold tracking-tighter leading-tight">
-          <Image alt={`Logo Bolso Extra`} src={`/images/bolsoextra.png`} height={120} width={200} />
+          <Image alt={`Logo Bolso Extra`} src={`/images/bolsoextra.png`} height={100} width={175} />
         </h1>
       </Link>
       <Navigation />
