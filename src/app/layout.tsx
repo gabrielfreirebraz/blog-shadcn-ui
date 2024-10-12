8184ab947ada3826f9a3f3d1fbf9c03d232cbfd6
Gabriel Braz <gabrielfreirebraz@gmail.com>
2024-10-12 16:37:20 -0300

    feat: new favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="BolsoExtra" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
+
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased max-w-6xl m-auto",
