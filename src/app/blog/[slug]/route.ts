671f6a5fcd2ee03b38470a062fcad6b9c64ab4be
Gabriel Braz <gabrielfreirebraz@gmail.com>
2024-10-21 00:48:12 -0300

    hotfix: redirect via route

---
+++ src/app/blog/[slug]/route.ts
@@ -0,0 +1,17 @@
+import { wisp } from "@/lib/wisp";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request, { params: { slug } }: { params: { slug: string } }) {
+  const post = await wisp.getPost(slug);
+  const categorySlug = post?.post?.tags[0]?.name;
+
+  if (categorySlug) {
+    const newUrl = new URL(`/${categorySlug}/${slug}`, request.url);
+    const response = NextResponse.redirect(newUrl.toString(), 301);
+    response.headers.set("x-robots-tag", "noindex, nofollow");
+
+    return response;
+  }
+
+  return new Response("Not Found", { status: 404 });
+}
