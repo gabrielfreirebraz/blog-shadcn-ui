83face415fc94348de7263a9e1ead0cc6a4d1d1a
Gabriel Braz <gabrielfreirebraz@gmail.com>
2024-11-16 23:47:01 -0300

    build(test): temp test in console

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -22,6 +22,9 @@ const authOptions = {
     secret: process.env.NEXTAUTH_SECRET,
 };
 
+console.log("GOOGLE_CLIENT_ID:", process.env.PRIVATE_GOOGLE_CLIENT_ID);
+console.log("GOOGLE_CLIENT_SECRET:", process.env.PRIVATE_GOOGLE_CLIENT_SECRET);
+console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
 
 const handler = NextAuth(authOptions);
 
