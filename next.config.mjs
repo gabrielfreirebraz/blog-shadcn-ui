1d7d66f82fe3ca2077837275e67175597affaa29
Gabriel Braz <gabrielfreirebraz@gmail.com>
2024-11-16 23:15:16 -0300

    hotfix(env): force configuration to load aws vars

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,11 @@ const nextConfig = {
   redirects: async () => {
     return [];
   },
+  env: {
+    PRIVATE_AWS_ACCESS_KEY_ID: process.env.PRIVATE_AWS_ACCESS_KEY_ID,
+    PRIVATE_AWS_SECRET_ACCESS_KEY: process.env.PRIVATE_AWS_SECRET_ACCESS_KEY,
+    PRIVATE_AWS_REGION: process.env.PRIVATE_AWS_REGION,
+  },
 };
 
 export default nextConfig;
--- src/lib/dynamoClient.ts
+++ src/lib/dynamoClient.ts
@@ -2,10 +2,10 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({
-  region: process.env._AWS_REGION ?? 'sa-east-1', 
+  region: process.env.PRIVATE_AWS_REGION ?? 'sa-east-1', 
   credentials: {
-    accessKeyId: process.env._AWS_ACCESS_KEY_ID ?? '',
-    secretAccessKey: process.env._AWS_SECRET_ACCESS_KEY ?? ''
+    accessKeyId: process.env.PRIVATE_AWS_ACCESS_KEY_ID ?? '',
+    secretAccessKey: process.env.PRIVATE_AWS_SECRET_ACCESS_KEY ?? ''
   }
 });
 
