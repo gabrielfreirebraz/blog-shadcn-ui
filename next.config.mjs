dd35a53458510708fd9638a6e4883e4491c10714
Gabriel Braz <gabrielfreirebraz@gmail.com>
2024-11-16 23:30:01 -0300

    hotfix(env): force configuration to load google vars

--- next.config.mjs
+++ next.config.mjs
@@ -11,10 +11,13 @@ const nextConfig = {
   redirects: async () => {
     return [];
   },
+  // force reload aws vars to build on production
   env: {
     PRIVATE_AWS_ACCESS_KEY_ID: process.env.PRIVATE_AWS_ACCESS_KEY_ID,
     PRIVATE_AWS_SECRET_ACCESS_KEY: process.env.PRIVATE_AWS_SECRET_ACCESS_KEY,
     PRIVATE_AWS_REGION: process.env.PRIVATE_AWS_REGION,
+    PRIVATE_GOOGLE_CLIENT_ID: process.env.PRIVATE_GOOGLE_CLIENT_ID,
+    PRIVATE_GOOGLE_CLIENT_SECRET: process.env.PRIVATE_GOOGLE_CLIENT_SECRET,
   },
 };
 
--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,8 @@ import GoogleProvider from "next-auth/providers/google";
 const authOptions = {
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+            clientId: process.env.PRIVATE_GOOGLE_CLIENT_ID ?? '',
+            clientSecret: process.env.PRIVATE_GOOGLE_CLIENT_SECRET ?? '',
         }),
         // FacebookProvider({
         //     clientId: process.env.FACEBOOK_CLIENT_ID ?? '',
