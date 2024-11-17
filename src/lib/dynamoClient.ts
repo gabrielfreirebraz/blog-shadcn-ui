89c8b62be1c4cad6833aedd1b3a06cd8a0c89ab6
Gabriel Braz <gabrielfreirebraz@gmail.com>
2024-11-16 21:49:18 -0300

    refactor(env): add prefix for private keys

--- src/lib/dynamoClient.ts
+++ src/lib/dynamoClient.ts
@@ -2,10 +2,10 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({
-  region: process.env.NEXT_PUBLIC_AWS_REGION ?? 'sa-east-1', 
+  region: process.env.PRIVATE_AWS_REGION ?? 'sa-east-1', 
   credentials: {
-    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID ?? '',
-    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY ?? ''
+    accessKeyId: process.env.PRIVATE_AWS_ACCESS_KEY_ID ?? '',
+    secretAccessKey: process.env.PRIVATE_AWS_SECRET_ACCESS_KEY ?? ''
   }
 });
 
