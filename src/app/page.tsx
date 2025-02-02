ab303e5489e3d5a3a2d57509b9c0b8af1e277cc4
Gabriel Braz <gabrielfreirebraz@gmail.com>
2025-02-02 18:48:59 -0300

    feat: new flag to banner to be fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import { BlogPostsPreview } from "@/components/BlogPostPreview";
 import { BlogPostsPagination } from "@/components/BlogPostsPagination";
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
+import { AdsSlot } from "@/features/ads/ui/ad-slot";
+import { GPTEnableServices } from "@/features/ads/ui/gpt-enable-services";
+import { GPTScript } from "@/features/ads/ui/gpt-script";
 import { wisp } from "@/lib/wisp";
 
 const Page = async ({
@@ -12,13 +15,24 @@ const Page = async ({
   const page = searchParams.page ? parseInt(searchParams.page as string) : 1;
   const result = await wisp.getPosts({ limit: 6, page });
 
+  const adBannerHasFixed = false;
+
   return (
-    <div className="container mx-auto px-5 mb-10">
-      <Header />
-      <BlogPostsPreview posts={result.posts} />
-      <BlogPostsPagination pagination={result.pagination} />
-      <Footer />
-    </div>
+    <>
+      <div className="container mx-auto px-5 mb-10">
+        <AdsSlot id="INTERNA-TOPO" fixed={adBannerHasFixed} />
+
+        {adBannerHasFixed && <><br /><br /><br /><br /></>}
+
+        <Header />
+        <BlogPostsPreview posts={result.posts} />
+        <BlogPostsPagination pagination={result.pagination} />
+        <Footer />
+      </div>
+
+      <GPTScript />
+      <GPTEnableServices />
+    </>
   );
 };
 
--- src/features/ads/ui/ad-slot.tsx
+++ src/features/ads/ui/ad-slot.tsx
@@ -7,9 +7,10 @@ import { useAdManager } from '../hooks';
 
 type TAdsSlotProps = {
   id: TGPTAdsConstantsKeys;
+  fixed?: boolean;
 };
 
-export function AdsSlot({ id }: Readonly<TAdsSlotProps>) {
+export function AdsSlot({ id, fixed }: Readonly<TAdsSlotProps>) {
   const ad = GPTAdsConstants[id];
 
   useAdManager({
@@ -17,19 +18,23 @@ export function AdsSlot({ id }: Readonly<TAdsSlotProps>) {
   });
 
   return (
-    <div className='flex flex-col items-center mt-1 md:mt-0 md:items-start'>
-      <span className='font-primary font-bold text-[6px] text-functional-heavy-light uppercase'>
-        publicidade
-      </span>
-      <div
-        id={`div-gpt-ad-${id.toLowerCase()}`}
-        data-testid={`div-gpt-ad-${id.toLowerCase()}`}
-        style={{
-          width: ad.sizes[0],
-          height: ad.sizes[1],
-          maxWidth: '100vw',
-        }}
-      />
+    <div className={`${fixed && 'fixed top-0 left-1/2 transform -translate-x-1/2 z-50'}`}>
+      <div className='flex flex-col items-center mt-1 md:mt-0 md:items-center'>
+        <span className='font-primary font-bold text-[6px] uppercase mt-4 text-[#9E9E9E]'>
+          publicidade
+        </span>
+
+        <div
+          id={`div-gpt-ad-${id.toLowerCase()}`}
+          data-testid={`div-gpt-ad-${id.toLowerCase()}`}
+          className='bg-gray-100'
+          style={{
+            width: ad.sizes[0],
+            height: ad.sizes[1],
+            maxWidth: '100vw',
+          }}
+        />
+      </div>
     </div>
   );
 }
